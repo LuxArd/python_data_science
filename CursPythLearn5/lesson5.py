2287dd739b42e753c6a57b6dcb063823028381bc
Victor <86299929+LuxArd@users.noreply.github.com>
2023-06-09 18:12:55 +0200

    "trecut o noua lectie  min, max, sum insert ... etc"

---
+++ CursPythLearn5/lesson5.py
@@ -0,0 +1,42 @@
+"""
+
+lst = [0,1,2,3,9]
+lst1 = range(0, 5)
+for lstr in lst1:
+    print(lstr)
+"""
+
+list = [1,2,3,4,5,66,88,99,2]
+list1 = [1, 2, 'car', 4, 9, 10]
+#list1.append(1001)
+#list1.append(['cars', 1111])
+#print(list1)
+#print(list1[3][0])
+#list1.clear()
+#print(list1)
+#lst33 = list.copy()
+#print(lst33)
+yx = [100005484]
+#list.append(yx)
+#print(list)
+#list.extend(yx)
+#print(list)
+#ttt = yx.copy()
+#print(ttt)
+#men = list.index(88)
+#print(men)
+#list.insert(3, 'caar')
+#print(list)
+#list.sort()
+#print(list)
+#coc = sorted(list)
+#print(coc)
+print(max(list))
+print(min(list))
+print(sum(list))
+
+
+
+
+
+
